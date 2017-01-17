ce9f48e75bb4dd471c26e5d5766ab9962d2b727e
Fabrice <fabrice.vaillant@centraliens.net>
2017-01-17 15:01:41 +0100

    Adds rune reading functions

--- lexer.go
+++ lexer.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 )
 
-type token int
+type tokenType int
 
 const (
 	// Operator on int
-	Plus token = iota
+	Plus tokenType = iota
 	Minus
 	Time
 
@@ -36,14 +36,51 @@ const (
 	Eof
 )
 
-type interpreter struct {
-	sigma map[string]int
-	prog  io.Reader
+var eof = rune(0)
+
+type Token struct {
+	ttype  tokenType
+	lexeme string
+	line   int
 }
 
-func newInterpreter(in io.Reader) *interpreter {
+type Interpreter struct {
+	sigma   map[string]int
+	prog    bufio.Reader
+	scanned []Token
+
+	line int
+}
+
+func newInterpreter(in bufio.Reader) *Interpreter {
 	sigma := make(map[string]int)
-	return &interpreter{sigma: sigma, prog: in}
+	scanned := make([]Token, 0)
+	return &Interpreter{sigma: sigma, prog: in, scanned: scanned, line: 0}
+}
+
+func (i *Interpreter) read() rune {
+	ch, _, err := i.prog.ReadRune()
+	if err != nil {
+		return eof
+	}
+	return ch
+}
+
+func (i *Interpreter) unread() {
+	_ = i.prog.UnreadRune()
+}
+
+func (i *Interpreter) peek() rune {
+	ch, _, err := i.prog.ReadRune()
+	if err != nil {
+		ch = eof
+	}
+	i.prog.UnreadRune()
+	return ch
+}
+
+func (i *Interpreter) scan() {
+
 }
 
 func main() {
