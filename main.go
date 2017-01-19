b5526bb4533b7a014603967c693d3add81b07b9b
Fabrice <fabrice.vaillant@centraliens.net>
2017-01-19 12:00:52 +0100

    Adds optionnal "prompt" mode

--- main.go
+++ main.go
@@ -9,17 +9,24 @@ import (
 
 func main() {
 	flag.Parse()
+	var in *bufio.Reader
 	args := flag.Args()
-	if len(args) != 1 {
-		fmt.Print("IMPinterpreter <source.imp>")
+	if len(args) == 0 {
+		in = bufio.NewReader(os.Stdin)
+	} else if len(args) == 1 {
+		source := args[0]
+		file, err := os.Open(source)
+		if err != nil {
+			panic(err.Error())
+		}
+		in = bufio.NewReader(file)
+	} else {
+		fmt.Println("IMPinterpreter <file> to interpret a file")
+		fmt.Println("if no file is provided, reads from stdin")
 		return
 	}
-	source := args[0]
-	file, err := os.Open(source)
-	if err != nil {
-		panic(err.Error())
-	}
-	l := NewLexer(bufio.NewReader(file))
+
+	l := NewLexer(in)
 	p := l.Scan()
 	i := NewInterpreter(p.parseInst())
 	i.execute()
